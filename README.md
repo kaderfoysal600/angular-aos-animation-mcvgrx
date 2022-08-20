5e70fa671045e7ac3c6b04edf44536a83911a945
kaderfoysal600 <lulaarbax@gmail.com>
2022-08-21 01:37:48 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-aos-animation-mcvgrx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-aos-animation-mcvgrx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos'; //AOS - 1
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  implements OnInit {
+  name = 'Angular 7 AOS Animations';
+  ngOnInit() {
+    AOS.init({disable: 'mobile'});//AOS - 2
+    AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+  }
+}
